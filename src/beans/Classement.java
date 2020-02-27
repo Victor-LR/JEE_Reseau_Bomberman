defe22abb437d8afbc789d7de291e194f46feebf
Leng <jerome.leng@etud.univ-angers.fr>
2020-02-27 19:01:20 +0100

    Simplification requete sql

--- src/beans/Classement.java
+++ src/beans/Classement.java
@@ -9,22 +9,24 @@ import java.util.ArrayList;
 
 public class Classement {
 
+	private Identifiant_BDD id;
 	private ArrayList<JoueurClassement> classementJournalier;
 	private ArrayList<JoueurClassement> classementMensuel;
 
 	public Classement() {
+		id = new Identifiant_BDD();
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			System.out.println(e.getMessage());
 		}
-		classementMensuel = executerCountMensuelV(Identifiant_BDD.getUrljdbc(), Identifiant_BDD.getUtilisateurBdd(),
-				Identifiant_BDD.getMotDePasseBdd());
-		classementJournalier = executerCountJournalierV(Identifiant_BDD.getUrljdbc(),
-				Identifiant_BDD.getUtilisateurBdd(), Identifiant_BDD.getMotDePasseBdd());
+		classementMensuel = executerCountV(Identifiant_BDD.getUrljdbc(), Identifiant_BDD.getUtilisateurBdd(),
+				Identifiant_BDD.getMotDePasseBdd(), "MONTH(date_partie)=MONTH(NOW())");
+		classementJournalier = executerCountV(Identifiant_BDD.getUrljdbc(), Identifiant_BDD.getUtilisateurBdd(),
+				Identifiant_BDD.getMotDePasseBdd(), "DAY(date_partie)=DAY(NOW())");
 	}
 
-	public ArrayList<JoueurClassement> executerCountMensuelV(String url, String utilisateur, String motDePasse) {
+	public ArrayList<JoueurClassement> executerCountV(String url, String utilisateur, String motDePasse, String tri) {
 		/* Connexion à la base de données */
 
 		Connection connexion = null;
@@ -36,11 +38,11 @@ public class Classement {
 
 			/* Création de l'objet gérant les requêtes */
 			statement = connexion.prepareStatement(
-					"SELECT pseudo_util , count(resultat) AS totalV FROM Historique WHERE (resultat='V' AND MONTH(date_partie)=MONTH(NOW())) GROUP BY pseudo_util ORDER BY totalv DESC;");
+					"SELECT pseudo_util , count(resultat) AS totalV FROM Historique WHERE (resultat='V' AND " + tri
+							+ ") GROUP BY pseudo_util ORDER BY totalv DESC;");
 			resultat = statement.executeQuery();
 			while (resultat.next()) {
 				JoueurClassement joueur = new JoueurClassement(resultat.getString(1), resultat.getInt(2));
-				System.out.println(resultat.getString(1) + " " + resultat.getInt(2));
 				listeUtilisateur.add(joueur);
 			}
 		} catch (SQLException e) {
@@ -65,52 +67,6 @@ public class Classement {
 				}
 			}
 		}
-		System.out.println("fin");
-		return listeUtilisateur;
-	}
-
-	public ArrayList<JoueurClassement> executerCountJournalierV(String url, String utilisateur, String motDePasse) {
-		/* Connexion à la base de données */
-
-		Connection connexion = null;
-		PreparedStatement statement = null;
-		ResultSet resultat = null;
-		ArrayList<JoueurClassement> listeUtilisateur = new ArrayList<>();
-		try {
-			connexion = DriverManager.getConnection(url, utilisateur, motDePasse);
-
-			/* Création de l'objet gérant les requêtes */
-			statement = connexion.prepareStatement(
-					"SELECT pseudo_util , count(resultat) AS totalV FROM Historique WHERE (resultat='V' AND DAY(date_partie)=DAY(NOW())) GROUP BY pseudo_util ORDER BY totalv DESC;");
-			resultat = statement.executeQuery();
-			while (resultat.next()) {
-				JoueurClassement joueur = new JoueurClassement(resultat.getString(1), resultat.getInt(2));
-				System.out.println(resultat.getString(1) + " " + resultat.getInt(2));
-				listeUtilisateur.add(joueur);
-			}
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		} finally {
-			if (resultat != null) {
-				try {
-					resultat.close();
-				} catch (SQLException ignore) {
-				}
-			}
-			if (statement != null) {
-				try {
-					statement.close();
-				} catch (SQLException ignore) {
-				}
-			}
-			if (connexion != null) {
-				try {
-					connexion.close();
-				} catch (SQLException ignore) {
-				}
-			}
-		}
-		System.out.println("fin");
 		return listeUtilisateur;
 	}
 
